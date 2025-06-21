9c006538fcc06841ad641fdb92d6d5ce4528f2ec
Razeen Shaheed <raze.priv@gmail.com>
2025-06-21 12:11:45 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'dbz-images.dubizzle.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
